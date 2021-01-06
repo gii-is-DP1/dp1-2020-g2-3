08e9ac738263f95ca3d92adc87ab7e1a52793bb0
Manuel Rivas <manolo.rivas.llamas@gmail.com>
2021-01-06 21:06:59 +0100

    crea trabajadores

--- src/main/java/org/springframework/samples/petclinic/web/TrabajadorController.java
+++ src/main/java/org/springframework/samples/petclinic/web/TrabajadorController.java
@@ -2,14 +2,18 @@ package org.springframework.samples.petclinic.web;
 
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.samples.petclinic.model.Trabajador;
 import org.springframework.samples.petclinic.service.TrabajadorService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -32,5 +36,22 @@ public class TrabajadorController {
 		modelMap.addAttribute("trabajadores", trabajadores);
 		return vista;
 	}
-
+	
+	@GetMapping("/new")
+	public String editNewTrabajador(ModelMap modelMap) {
+		modelMap.addAttribute("trabajador",new Trabajador());
+		return "trabajadores/updateTrabajadorForm";
+	} 
+	
+	@PostMapping("/new")
+	public String saveNewTrabajador(@Valid Trabajador trabajador, BindingResult binding, ModelMap modelMap) {
+		if(binding.hasErrors()) {
+			modelMap.put("trabajador", trabajador);
+			return "trabajadores/updateTrabajadorForm";
+		}else {
+			trabajadorService.save(trabajador);
+			modelMap.addAttribute("message","Trabajador creado correctamente");
+			return listadoTrabajadores(modelMap);
+		}
+}
 }
