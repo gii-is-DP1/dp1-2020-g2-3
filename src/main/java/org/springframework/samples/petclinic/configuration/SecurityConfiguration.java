3709823a6f407ff74b98b74bc7271439be5d200c
joscuegal1 <61007947+joscuegal1@users.noreply.github.com>
2021-01-06 20:32:44 +0100

    Revert "Manrivlla"

--- src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
+++ src/main/java/org/springframework/samples/petclinic/configuration/SecurityConfiguration.java
@@ -43,7 +43,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 				.antMatchers("/clientes/**").hasAnyAuthority("admin","taxista")
 				.antMatchers("/reservas/**").authenticated()
 				.antMatchers("/trabajadores/**").hasAnyAuthority("admin")
-				.antMatchers("/reservas/**").permitAll()
 				.anyRequest().denyAll()
 				.and()
 				 	.formLogin()
