2d45785c1704ad0f13de830180589d73bc33dd09
JosanFCS03 <josanfcs98@hotmail.com>
2021-02-01 18:43:45 +0100

    Cambio de import

--- src/main/java/org/springframework/samples/petclinic/service/ReservaService.java
+++ src/main/java/org/springframework/samples/petclinic/service/ReservaService.java
@@ -2,6 +2,7 @@ package org.springframework.samples.petclinic.service;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
